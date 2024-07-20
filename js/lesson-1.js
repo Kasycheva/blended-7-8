ede2d0a622b0d46311cef81dc4d5010b58213d10
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2024-07-20 14:23:06 +0200

    task-1

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+//* Розгалуження: if, if...else, else...if
+//TODO: 1 ===================================
+//Даний рядок, що складається із символів, наприклад, 'abcde'.
+// Перевірте, що першим символом цього рядка є буква 'a'.
+// Якщо це так - виведіть 'так', інакше виведіть 'ні'.
+// const str = 'Abcde';
+// if (str.toLocaleLowerCase().startsWith('a')) {
+//     console.log('yes'); 
+// } else {
+//   console.log('no');   
+// }
+// str.toLocaleLowerCase()[0] === 'a' ? console.log('yes') : console.log('no');
