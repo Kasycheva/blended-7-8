58d535fecef6c2e3b2cd1afa93920a6c41ef3b89
Sairus78 <83472347+Sairus78@users.noreply.github.com>
2024-08-03 15:03:20 +0300

    task1

---
+++ js/lesson-2.js
@@ -0,0 +1,25 @@
+// ! Масиви і функції
+//TODO:=========task-01=================
+// Створіть масив styles з елементами «Джаз» та «Блюз».
+// Додайте "Рок-н-рол" в кінець.
+// Замініть значення "Блюз" на "Класика".
+// Видалить перший елемент масиву і виведіть його в консоль.
+// Вставте «Реп» і «Реггі» на початок масиву.
+
+// const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-рол');
+// styles.shift();
+// // styles[1] = 'Класика';
+// styles.unshift('Реп', 'Реггі');
+// const index = styles.indexOf('Блюз');
+// console.log(index);
+// if (index !== -1) {
+//   styles[index] = 'Класика';
+// }
+// // console.log(styles);
+// // for (const style of styles) {
+// //   console.log(style);
+// // }
+// for (let index = 0; index < styles.length; index++) {
+//   console.log(`${index + 1} : ${styles[index]}`);
+// }
