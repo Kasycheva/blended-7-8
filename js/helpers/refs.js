01ffe7f6e416b861ac3a5d2b56969986cf030270
DenVik <denyagarkusha2@gmail.com>
2024-09-14 16:03:13 +0200

    add lesson 5

--- js/helpers/refs.js
+++ js/helpers/refs.js
@@ -1,4 +1,13 @@
 export const refs = {
   checkbox: document.querySelector('#theme-switch'),
   body: document.body,
+  month: document.querySelector('.js-month'),
+  day: document.querySelector('.js-day-number'),
+  weekDay: document.querySelector('.js-day'),
+  year: document.querySelector('.js-year'),
+  ageInput: document.querySelector('.js-input'),
+  ageButton: document.querySelector('.js-form-btn'),
+  ageResult: document.querySelector('.js-form-result'),
 };
+
+
--- js/lesson-5.js
+++ js/lesson-5.js
@@ -27,3 +27,81 @@ function renderPage() {
     refs.body.classList.replace('dark', 'light');
   }
 }
+
+const currentDate = new Date()
+const month = currentDate.toLocaleDateString("uk-UA", {month:"long"});
+const day = currentDate.toLocaleDateString("uk-UA", {day: "numeric"});
+const weekDay = currentDate.toLocaleDateString("uk-UA", {weekday: "long"});
+const year = currentDate.toLocaleDateString("uk-UA", {year: "numeric"});
+
+refs.month.textContent = month;
+refs.day.textContent = day;
+refs.weekDay.textContent = weekDay;
+refs.year.textContent = year;
+
+//age-calculator
+
+refs.ageButton.addEventListener('click', calculateAge);
+
+function calculateAge (){
+	const userDate = new Date(refs.ageInput.value);
+	const currentDate = new Date();
+	const different = currentDate - userDate;
+
+	const result = getAge(different)
+	refs.ageResult.textContent = `Your age is ${result} years old`
+}
+
+function getAge(ms){
+return Math.floor(ms/(1000 * 60 * 60 * 24 * 365.25))
+}
+
+// setTimeout(()=>{
+// console.log(1);
+
+// }, 1000)
+
+// setTimeout(()=>{
+// 	console.log(2);
+	
+// }, 500);
+
+// setTimeout(()=>{
+// 	console.log(4);
+	
+// }, 0);
+
+// console.log(3);
+
+
+/**
+ * - Використовуй prompt і повертай значення звідти.
+ * - Створи функцію, всередині якої буде проміс.
+ * Якщо значення не є числом, відхиляй проміс і логіруй "error".
+ * Якщо значення парне, вирішуй проміс і повертай "even" через 1 секунду.
+ * Якщо значення не парне, вирішуй проміс і повертай "odd" через 2 секунди.
+ */
+
+const value = prompt('past value');
+
+function checkValue (valueInner){
+return new Promise((resolve, reject) => {
+	if(isNaN(valueInner)){
+		return reject("error")
+	}
+	if (valueInner % 2 === 0){
+		setTimeout(()=>{
+			return resolve("even")
+		}, 1000)
+	}
+	setTimeout(()=>{
+		return resolve("odd")
+	}, 2000)
+})
+}
+
+checkValue (value).then(response => {
+	console.log(response)
+}).catch(error => {
+	console.log(error)
+})
